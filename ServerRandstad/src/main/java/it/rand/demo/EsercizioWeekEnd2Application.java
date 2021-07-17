e78eaaa9605441ac0a9b9eebe94fc242fcb1a10a
Andrea Paciocca <81762546+AndreaPac@users.noreply.github.com>
2021-07-17 15:46:44 +0200

    Implementato Server e Client

---
+++ ServerRandstad/src/main/java/it/rand/demo/EsercizioWeekEnd2Application.java
@@ -0,0 +1,13 @@
+package it.rand.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioWeekEnd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioWeekEnd2Application.class, args);
+	}
+
+}
---
+++ ServerRandstad/src/test/java/it/rand/demo/EsercizioWeekEnd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.rand.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioWeekEnd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
